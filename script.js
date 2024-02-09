80dea8cee7760f12b0f525d88be7797696b2d395
Sourin Mukherjee <98275372+i-am-faith@users.noreply.github.com>
2024-02-10 05:04:10 +0530

    Added files via upload

--- script.js
+++ script.js
@@ -3,68 +3,68 @@ function sendBTC() {
     var name = "BTC-USD";
 
     // Redirect to the receiver page with the name as a parameter
-    window.location.href = 'https://switch-to-bit.streamlit.app/?name=' + encodeURIComponent(name);
+    window.open('https://switch-to-bit.streamlit.app/?name=' + encodeURIComponent(name), '_blank');
 }
 function sendDOGE() {
     // Get the name
     var name = "DOGE-USD";
 
     // Redirect to the receiver page with the name as a parameter
-    window.location.href = 'https://switch-to-bit.streamlit.app/?name=' + encodeURIComponent(name);
+    window.open('https://switch-to-bit.streamlit.app/?name=' + encodeURIComponent(name), '_blank');
 }
 function sendSHB() {
     // Get the name
     var name = "SHIB-USD";
 
     // Redirect to the receiver page with the name as a parameter
-    window.location.href = 'https://switch-to-bit.streamlit.app/?name=' + encodeURIComponent(name);
+    window.open('https://switch-to-bit.streamlit.app/?name=' + encodeURIComponent(name), '_blank');
 }
 function sendBNC() {
     // Get the name
     var name = "BNB-USD";
 
     // Redirect to the receiver page with the name as a parameter
-    window.location.href = 'https://switch-to-bit.streamlit.app/?name=' + encodeURIComponent(name);
+    window.open('https://switch-to-bit.streamlit.app/?name=' + encodeURIComponent(name), '_blank');
 }
 function sendSOL() {
     // Get the name
     var name = "SOL-USD";
 
     // Redirect to the receiver page with the name as a parameter
-    window.location.href = 'https://switch-to-bit.streamlit.app/?name=' + encodeURIComponent(name);
+    window.open('https://switch-to-bit.streamlit.app/?name=' + encodeURIComponent(name), '_blank');
 }
 function sendNEO() {
     // Get the name
     var name = "NEO-USD";
 
     // Redirect to the receiver page with the name as a parameter
-    window.location.href = 'https://switch-to-bit.streamlit.app/?name=' + encodeURIComponent(name);
+    window.open('https://switch-to-bit.streamlit.app/?name=' + encodeURIComponent(name), '_blank');
 }
 function sendLITE() {
     // Get the name
     var name = "LTC-USD";
 
     // Redirect to the receiver page with the name as a parameter
-    window.location.href = 'https://switch-to-bit.streamlit.app/?name=' + encodeURIComponent(name);
+    window.open('https://switch-to-bit.streamlit.app/?name=' + encodeURIComponent(name), '_blank');
 }
 function sendPOL() {
     // Get the name
     var name = "MATIC-USD";
 
     // Redirect to the receiver page with the name as a parameter
-    window.location.href = 'https://switch-to-bit.streamlit.app/?name=' + encodeURIComponent(name);
+    window.open('https://switch-to-bit.streamlit.app/?name=' + encodeURIComponent(name), '_blank');
 }
 function sendETH() {
     // Get the name
     var name = "ETH-USD";
 
     // Redirect to the receiver page with the name as a parameter
-    window.location.href = 'https://switch-to-bit.streamlit.app/?name=' + encodeURIComponent(name);
+    window.open('https://switch-to-bit.streamlit.app/?name=' + encodeURIComponent(name), '_blank');
 }
 function sendMON() {
     // Get the name
     var name = "XMR-USD";
 
     // Redirect to the receiver page with the name as a parameter
-    window.location.href = 'https://switch-to-bit.streamlit.app/?name=' + encodeURIComponent(name);
+    window.open('https://switch-to-bit.streamlit.app/?name=' + encodeURIComponent(name), '_blank');
 }
