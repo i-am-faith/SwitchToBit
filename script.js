21c61cef2c399095863cd8b496040f14b7a16d66
Sourin Mukherjee <98275372+i-am-faith@users.noreply.github.com>
2024-02-10 05:32:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,22 @@ function sendBTC() {
     // Get the name
     var name = "BTC-USD";
 
-    // Send a message to the target website with the name as data
-    window.postMessage(name, 'http://localhost:8501/');
+    // Redirect to the receiver page with the name as a parameter
+    var url = 'https://switch-to-bit.streamlit.app/?name=' + encodeURIComponent(name);
+
+    // Open the target webpage in a new tab
+    var newTab = window.open(url, '_blank');
+
+    // Check if the new tab is opened successfully
+    if (newTab) {
+        // Wait for a short time to ensure the new page is loaded (adjust as needed)
+        setTimeout(function () {
+            // Access the input field on the target webpage and set its value
+            newTab.document.querySelector('.st-c5.st-ct.st-cu.st-cv.st-cw.st-cx.st-cy.st-cz.st-d0.st-d1.st-ba.st-c7.st-d2.st-d3.st-d4.st-d5.st-d6.st-d7.st-d8.st-d9.st-bt.st-bu.st-bv.st-da.st-bx.st-by.st-bs.st-db.st-dc.st-dd').value = name;
+        }, 10000); // Adjust the timeout value as needed
+    }
 }
+
 
 function sendDOGE() {
     // Get the name
